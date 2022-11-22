49f0773c8efe0797edcb9e653e7f34ffda7a4045
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-11-22 18:12:09 +0200

    Connected the Weather API

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+﻿const wrapper = document.querySelector(".wrapper"), 
+    inputPart = wrapper.querySelector(".input-part"),
+    infoTxt = inputPart.querySelector(".info-txt");
+
+
+const inputField = document.getElementById("input");
+
+inputField.addEventListener('keyup', e => {
+    if(e.key == "Enter" && inputField.value != ""){
+        requestApi(inputField.value);
+    }
+})
+
+function requestApi(city){
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=69d455ec939d65d892bb538eae6bf382`;
+    
+    infoTxt.innerText = "Loading...";
+    infoTxt.classList.add("pending");
+    
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info){
+    console.log(info);
+}
